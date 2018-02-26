f78ac406c5c8970bf3c7025ddcb7cecadaf9e1f7
Ollie <oliverdhayden@gmail.com>
2018-02-26 19:12:32 +0000

    added file upload activity

---
+++ app/src/main/java/com/groupl/project/pier/FileUpload.java
@@ -0,0 +1,13 @@
+package com.groupl.project.pier;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_file_upload extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_file_upload);
+    }
+}
