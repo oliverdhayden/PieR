9d0e8a6fb897d4526c95af9e6e4081ff6388203d
Raju Minahjul Abedin <rajuminhajulabedin@gmail.com>
2018-02-26 16:25:07 +0000

    Raju - Fix slider

--- app/src/main/java/com/groupl/project/pier/WelcomePage.java
+++ app/src/main/java/com/groupl/project/pier/WelcomePage.java
@@ -7,46 +7,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-import static com.groupl.project.pier.WelcomePage.setPreference;
-
 public class WelcomePage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if(getPreference(this,"firsttime")) {
-            setContentView(R.layout.welcome_screen0);
-        }
-        else {
-            Intent intent = new Intent(WelcomePage.this, MainActivity.class);
-            startActivity(intent);
-            setPreference(false, "firsttime",this);
-        }
-
-
     }
 
 
-    public void skipTour(View view) {
-        Intent intent = new Intent(WelcomePage.this, MainActivity.class);
-        startActivity(intent);
-        setPreference(false, "firsttime",this);
-    }
 
-    static public void setPreference(boolean b, String option,Context context) {
-        SharedPreferences prefs = context.getSharedPreferences("Preference", MODE_PRIVATE);
-        SharedPreferences.Editor editor = prefs.edit();
-        editor.putBoolean("Option " + option, b);
-        editor.apply();
-    }
-    static public boolean getPreference(Context context, String option){
-        SharedPreferences prefs = context.getSharedPreferences("Preference", MODE_PRIVATE);
-        return prefs.getBoolean("Option "+ option, true);
-    }
-
-    public void startTour(View view) {
-        Intent intent = new Intent(WelcomePage.this, WelcomeSlider.class);
-        startActivity(intent);
-        setPreference(false, "firsttime",this);
-    }
 }
--- app/src/main/java/com/groupl/project/pier/WelcomeSlider.java
+++ app/src/main/java/com/groupl/project/pier/WelcomeSlider.java
@@ -1,6 +1,8 @@
 package com.groupl.project.pier;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,6 +16,34 @@ public class WelcomeSlider extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(getPreference(this,"firsttime")) {
+            setContentView(R.layout.welcome_screen0);
+        }
+        else {
+            Intent intent = new Intent(WelcomeSlider.this, MainActivity.class);
+            startActivity(intent);
+            setPreference(false, "firsttime",this);
+            finish();
+        }
+    }
+    static public void setPreference(boolean b, String option,Context context) {
+        SharedPreferences prefs = context.getSharedPreferences("Preference", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putBoolean("Option " + option, b);
+        editor.apply();
+    }
+    static public boolean getPreference(Context context, String option){
+        SharedPreferences prefs = context.getSharedPreferences("Preference", MODE_PRIVATE);
+        return prefs.getBoolean("Option "+ option, true);
+    }
+    public void skipTour(View view) {
+        Intent intent = new Intent(WelcomeSlider.this, MainActivity.class);
+        startActivity(intent);
+        setPreference(false, "firsttime",this);
+        finish();
+    }
+
+    public void startTour(View view) {
         setContentView(R.layout.activity_welcome_slider);
         viewPager = (ViewPager) findViewById(R.id.viewpager);
         myadapter = new SlideAdapter(this);
