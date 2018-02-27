5278c2bef16caab4654d815ce374ccfcd640b536
Harley Wright <hwrig002@gold.ac.uk>
2018-02-27 14:09:14 +0000

    please don't break everything

--- app/src/main/java/com/groupl/project/pier/FileUpload.java
+++ app/src/main/java/com/groupl/project/pier/FileUpload.java
@@ -11,12 +11,12 @@ import android.app.Activity;
 
 
 import android.util.Log;
-//import com.amazonaws.mobile.client.AWSMobileClient;
-//import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
-//import com.amazonaws.mobileconnectors.s3.transferutility.TransferState;
-//import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
-//import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener;
-//import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferState;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener;
+import com.amazonaws.services.s3.AmazonS3Client;
 
 
 import java.io.File;
