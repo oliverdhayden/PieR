8d26ac4516c08eb8ce40659831905a2dfe867db3
alexandra <elenapricopp@gmail.com>
2018-04-22 17:15:46 +0100

    Update aboutUs page

--- app/src/main/java/com/groupl/project/pier/aboutUS.java
+++ app/src/main/java/com/groupl/project/pier/aboutUS.java
@@ -1,7 +1,10 @@
 package com.groupl.project.pier;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class aboutUS extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class aboutUS extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+        ImageButton goBack = (ImageButton) findViewById(R.id.buttonToGoBackFromAboutUsPage);
+        goBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent0 = new Intent(aboutUS.this, MainActivity.class);
+                startActivity(intent0);
+            }
+        });
     }
 }
